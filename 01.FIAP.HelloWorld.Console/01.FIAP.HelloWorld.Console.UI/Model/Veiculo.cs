370d0cbfc338060aac85fe77f26b9e323a65cca9
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-03 20:45:25 -0300

    01 - Introdução

---
+++ 01.FIAP.HelloWorld.Console/01.FIAP.HelloWorld.Console.UI/Model/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.FIAP.HelloWorld.Console.UI.Model
+{
+    public abstract class Veiculo
+    {
+        public double Potencia { get; set; }
+
+        public int Ano { get; set; }
+    }
+}
---
+++ 01.FIAP.HelloWorld.Console/01.FIAP.HelloWorld.Console.UI/Program.cs
@@ -0,0 +1,29 @@
+﻿using _01.FIAP.HelloWorld.Console.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.FIAP.HelloWorld.Console.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+
+            carro.Ano = 2010;
+            carro.ArCondicionado = false;
+
+            System.Console.WriteLine(carro.Ano);
+
+            var lancha = new Lancha()
+            {
+                Ano = 2010,
+                BoiaSalvaVida = true,
+                Potencia = 150
+            };
+        }
+    }
+}
