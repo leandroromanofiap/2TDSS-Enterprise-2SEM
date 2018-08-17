1c45d212c28b272e3092f1e4ef7055fba5c2cea1
Leandro Romano <rm75244@fiap.com.br>
2018-08-17 20:44:24 -0300

    Razor Tags

---
+++ 04.Fiap.Web.MVC/04.Fiap.Web.MVC/Controllers/MedicoController.cs
@@ -0,0 +1,37 @@
+﻿using _04.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04.Fiap.Web.MVC.Controllers
+{
+    public class MedicoController : Controller
+    {
+        // Simulação de banco de dados.
+        private static List<Medico> _banco = new List<Medico>();
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            return View(_banco);
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Medico medico)
+        {
+            _banco.Add(medico);
+
+            TempData["mensagem"] = "Médico cadastrado com sucesso.";
+
+            return RedirectToAction("Cadastrar");
+        }
+    }
+}
---
+++ 04.Fiap.Web.MVC/04.Fiap.Web.MVC/Models/Medico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _04.Fiap.Web.MVC.Models
+{
+    public class Medico
+    {
+        public int Crm { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Salario { get; set; }
+    }
+}
